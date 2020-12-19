1261e69870679fbfa1a15c80c9bbda5f4f8c4e29
vsRushy <vsRushy@users.noreply.github.com>
2020-12-19 16:45:10 +0100

    Correction on timestamp.

--- VisualDataAnalysis/Assets/Scripts/ReaderCSV.cs
+++ VisualDataAnalysis/Assets/Scripts/ReaderCSV.cs
@@ -71,10 +71,24 @@ public class ReaderCSV : MonoBehaviour
 
         for (int row = 0; row < nrows; ++row)
         {
+            /*
+             * 
+                When calling the constructor of Eventinfo, a timestamp is automatically generated which stores the seconds since startup.
+                Therefore, when we want to read an EventInfo, we will:
+                    1) Store the actual timestamp of that event.
+                    2) Call the constructor (which will generate the timestamp, and it will be wrong, because it will not be the actual previous timestamp).
+                    3) Rewrite that timestamp for the correct one, which we stored in step 1.
+
+                // Will be improved to a better option. It only rewrites the String time, not the seconds nor time_stamp variables.
+             * 
+             */
+
             //Create (NOTE: Usage of another constructor of Eventinfo)
+            String time_event = data[row][3];
             Eventinfo n_event = new Eventinfo(data[row][0], uint.Parse(data[row][1]), (CUSTOM_EVENT_TYPE)Enum.Parse(typeof(CUSTOM_EVENT_TYPE), data[row][2]),
                 new Vector3(float.Parse(data[row][4]), float.Parse(data[row][5]), float.Parse(data[row][6])),
                 uint.Parse(data[row][7]));
+            n_event.time = time_event;
 
             //Add row info
             selectedList.Add(n_event);
